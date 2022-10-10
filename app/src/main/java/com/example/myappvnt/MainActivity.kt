26dfd22954f5266293444f43b2629ffc62d51d81
Victor Ferrari <victorferrari2k7@gmail.com>
2022-10-10 19:31:23 -0300

    Implementando layout com RecycleView

--- app/src/main/java/com/example/myappvnt/MainActivity.kt
+++ app/src/main/java/com/example/myappvnt/MainActivity.kt
@@ -2,12 +2,15 @@ package com.example.myappvnt
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.LinearLayout
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val counterApplication = findViewById<LinearLayout>(R.id.counter_application)
 
     }
 
